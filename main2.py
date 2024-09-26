7d27b2aa0b825c8b5a8a6180348e2feb33bfed9d
nirvik saha <nsdev404@gmail.com>
2024-09-26 10:31:32 -0400

    project updates

--- main2.py
+++ main2.py
@@ -54,42 +54,6 @@ def update_worksheet_entry(user_question="null", bot_answer="null"):
     sheet.append_row(x)
 
 
-def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
-
-
-def get_text_chunks(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-    )
-    chunks = text_splitter.split_text(text)
-    return chunks
-
-
-def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings()
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    return vectorstore
-
-
-def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
-    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
-    )
-    return conversation_chain
-
 
 def handle_userinput(user_question):
     try:
@@ -136,16 +100,11 @@ def run_main():
                     for uploaded_file in pdf_docs:
                         filenames.append(uploaded_file.name)
                     st.session_state.filenames =  ", ".join(filenames)
-                    raw_text = get_pdf_text(pdf_docs)
-                    text_chunks = get_text_chunks(raw_text)
-                    vectorstore = get_vectorstore(text_chunks)
-                    st.session_state.conversation = get_conversation_chain(vectorstore)
+
 
 
 if __name__ == '__main__':
     run_main()
-    ### for streamlit use
-    # conn = st.connection("gsheets", type=GSheetsConnection)
-    # df = conn.read(worksheet="Sheet1", usecols=list(range(3)))
+
 
     
